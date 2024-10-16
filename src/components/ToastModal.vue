83d9e36ff2ba9241e12b1acd650731760fb2a14f
不知火 Shiranui <supersonic@livemail.tw>
2024-10-16 18:15:51 +0800

    feat: upgrade to vue3.5

---
+++ src/components/ToastModal.vue
@@ -0,0 +1,43 @@
+<template>
+  <button
+    v-show="model"
+    type="button"
+    class="fixed right-2 lg:right-24 top-24 z-50 rounded-md px-4 py-2 text-white transition bg-amber-500 hover:bg-amber-600 animate-fade"
+    @click="handleClickClose"
+  >
+    <div class="flex items-center space-x-2">
+      <span>
+        <information-circle-icon class="h-5 w-5" />
+      </span>
+      <span class="font-bold">
+        {{ model }}
+      </span>
+    </div>
+  </button>
+</template>
+
+<script setup>
+import { watch } from "vue";
+
+import {
+  InformationCircleIcon,
+} from "@heroicons/vue/24/solid";
+
+let timer;
+
+const model = defineModel({
+  type: String,
+});
+
+const handleClickClose = () => {
+  if (timer) {
+    clearTimeout(timer);
+  }
+  model.value = "";
+}
+
+watch(model, () => {
+  if (!model.value) return;
+  timer = setTimeout(handleClickClose, 3000);
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,11 +5,24 @@
         <h1 class="text-4xl tracking-tight font-extrabold text-gray-900">
           <span class="block xl:inline">這裡是</span>
           <span class="block text-indigo-600 xl:inline">前端 Web 模板</span>
+          <span class="block xl:inline">
+            <loading-circle-icon
+              class="inline-block h-8 w-8 ml-3 animate-spin text-indigo-300"
+            />
+          </span>
         </h1>
         <p class="mt-3 text-base text-gray-500">
           Vite + Vue.js 3 + Tailwind CSS，為了快速發展專案而生，請自行修改內容。
         </p>
         <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+          <div class="rounded-md shadow">
+            <button
+              class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+              @click="onClickHello"
+            >
+              Hello!
+            </button>
+          </div>
           <div class="mt-3 sm:mt-0 sm:ml-3">
             <a
               href="https://github.com/web-tech-tw/template.inte"
@@ -44,10 +57,18 @@
       </div>
     </div>
   </div>
+  <toast-modal v-model="toastMessage" />
 </template>
 
-<script>
-export default {
-  name: 'HomeView',
-}
+<script setup>
+import { ref } from 'vue';
+
+import LoadingCircleIcon from '../components/LoadingCircleIcon.vue';
+import ToastModal from '../components/ToastModal.vue';
+
+const toastMessage = ref('');
+
+const onClickHello = () => {
+  toastMessage.value = 'Hello!';
+};
 </script>
