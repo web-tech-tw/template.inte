d8400f2bdca2b0953fea6299cb06e5042d751f77
不知火 Shiranui <supersonic@livemail.tw>
2025-11-10 05:31:13 +0800

    refactor: as wings

---
+++ app/components/DynamicHeroIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <UIcon
+    :name="props.name"
+    :class="props.class"
+  />
+</template>
+
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  class: {
+    type: String,
+    required: false,
+    default: () => "",
+  },
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
+  css: ['~/assets/css/main.css'],
   modules: [
     '@nuxt/eslint',
     '@nuxt/image',
